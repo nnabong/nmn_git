21cc15bad16aeba37eb673d86f530dd13b546f9a
nnabong <110580100+nnabong@users.noreply.github.com>
2023-02-14 14:21:54 +0800

    Update prod1.ps1

--- AMIS/product/prod1/common/scripts/prod1.ps1
+++ AMIS/product/prod1/common/scripts/prod1.ps1
@@ -8,5 +8,6 @@ Write-Host "$($PWD)"
 Write-Host "This is Prod1"
 
 #$csvfile = import-csv amis/product/prod2/prod2.csv
-$csvfile = import-csv $workingDirectory
-$csvfile
+
+Write-Host "Imported data below:"
+$workingDirectory
