178523f9130e964fd750ea8491c211c634f1be02
nnabong <110580100+nnabong@users.noreply.github.com>
2023-02-14 13:37:13 +0800

    Update prod1.ps1

--- AMIS/product/prod1/common/scripts/prod1.ps1
+++ AMIS/product/prod1/common/scripts/prod1.ps1
@@ -2,6 +2,6 @@ Write-Host "$($PWD)"
 Write-Host "This is Prod1"
 
 #$csvfile = import-csv amis/product/prod2/prod2.csv
-$csvfile = import-csvD:\a\nmn_git\nmn_git\AMIS\product\prod2\prod2.csv
+$csvfile = import-csv "D:\a\nmn_git\nmn_git\AMIS\product\prod2\prod2.csv"
 
 $csvfile
